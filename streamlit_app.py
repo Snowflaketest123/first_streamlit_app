c94e07e127e696b1f202ae910e3eb06936fd1b8f
Snowflaketest123 <108739149+Snowflaketest123@users.noreply.github.com>
2022-07-08 11:45:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 import pandas
 import requests
-
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 fruityjuice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 #load Data from txt into Variable my_fruit_list
